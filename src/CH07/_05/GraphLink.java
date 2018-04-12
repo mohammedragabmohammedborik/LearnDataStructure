43e5735266165ddeeb2853fc653e65e82c0993bd
lilinxiong <lilinxiong1997@gmail.com>
2018-04-12 20:07:43 +0800

    图形结构:先广后深遍历(未完成)

---
+++ src/CH07/_05/GraphLink.java
@@ -0,0 +1,16 @@
+package CH07._05;
+
+/**
+ * Create by xong on 2018/4/12
+ */
+class Node {
+    int value;
+    Node next;
+    public Node(int value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+public class GraphLink {
+
+}
